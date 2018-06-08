914643c835b465bc54d769a5899135b57c51775a
MhFchrulys <39640895+MhFchrulys@users.noreply.github.com>
2018-06-08 08:05:42 +0700

    Add files via upload
    Tugas Struktur Data Maksimum Minimum Pelatuk -1

---
+++ Max_Min.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaex1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class Max_Min {
+    public static void main(String[] args) {
+        System.out.println("Mencari Nilai Minimum Dan Maksimum pada Index Array dengan -1 sebagai pelatuknya");
+        Scanner sc = new Scanner(System.in);
+        int x[] = new int[100];
+        int jml = 100,stop = 0;
+        for (int i=0;i<jml;i++){
+            System.out.print("Masukkan Array isi " + (i+1) + " = ");
+            x[i] = sc.nextInt();
+                if(x[i]==-1){
+                  jml = -1;
+                  stop = i;   
+            }
+        }
+        int  maks = x[0],min = x[0];
+        for (int i=0;i<stop;i++){
+            if (x[i]>maks){
+                maks = x[i];
+            }
+            else if(x[i]< min) {
+                min = x[i];
+            }
+        }
+        
+        System.out.println("");
+        System.out.println("Nilai Maksimumnya : " + maks);
+        System.out.println("Nilai Minimumnya : " + min);
+    }
+}
